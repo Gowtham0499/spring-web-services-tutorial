408ce9f3ce725937e4684dd011c7dc1bf772fb17
Gowtham0499 <gowth0499@gmail.com>
2021-02-05 10:32:18 +0530

    Initial Commit

---
+++ sampleRestWebService/src/main/java/com/rest/webservice/app/SampleRestWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rest.webservice.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleRestWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleRestWebServiceApplication.class, args);
+	}
+
+}
---
+++ sampleRestWebService/src/test/java/com/rest/webservice/app/SampleRestWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.webservice.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleRestWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
