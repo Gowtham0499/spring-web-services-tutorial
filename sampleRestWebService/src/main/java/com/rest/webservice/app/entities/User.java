5dd04d1915541bd1ecb3a2388f59ce722e0a4bad
Gowtham0499 <gowth0499@gmail.com>
2021-02-05 13:56:11 +0530

    Added Swagger Support

--- sampleRestWebService/src/main/java/com/rest/webservice/app/entities/User.java
+++ sampleRestWebService/src/main/java/com/rest/webservice/app/entities/User.java
@@ -5,12 +5,18 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel
 public class User {
 
 	private Integer id;
 	@Size(min = 2, message = "Name should be atleast 2 characters!")
+	@ApiModelProperty(notes = "Name should be atleast 2 characters")
 	private String name;
 	@Past
+	@ApiModelProperty(notes = "Date should only be in past")
 	private Date birthDate;
 
 	public User(Integer id, String name, Date birthDate) {
